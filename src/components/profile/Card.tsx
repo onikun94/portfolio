cc5fa17c8458699d5cfa105e18e83a9e6a1beca3
onikun94 <onikun94@gmail.com>
2022-05-30 07:21:00 +0900

    manage profData for array

--- src/components/profile/Card.tsx
+++ src/components/profile/Card.tsx
@@ -2,23 +2,19 @@ import Image from 'next/image';
 import React, { VFC } from 'react';
 
 type cardPropsType = {
-  content1: string;
+  // content1: string;
   size1?: string;
-  content2: string;
-  content3?: string;
-  content4?: string;
-  content5?: string;
-  content6?: string;
+  profContents: { id: string; content: string }[];
 };
-const Card: VFC<cardPropsType> = (props) => {
+const Card: VFC<cardPropsType> = ({ profContents }) => {
   return (
     <p>
       <div className="p-2 my-4">
         <div className="flex justify-end">
-          <div className=" mx-16">
+          <div className=" mx-16 my-8">
             <Image
-              className=" w-2/4 rounded-full"
-              src="/onikun.png"
+              className=" w-2/4 rounded-full "
+              src="/onikun1.png"
               width={200}
               height={200}
               decoding="async"
@@ -27,13 +23,16 @@ const Card: VFC<cardPropsType> = (props) => {
           </div>
           <div className="m-2 w-2/3">
             <div className="p-4 ">
-              <p className="p-1 text-base">{props.content1}</p>
-              <p className="text-xs   ">{props.content2}</p>
+              {/* <p className="p-1 text-base">{props.content1}</p>
+              <p className="text-xs   ">{props.content2}</p> */}
             </div>
-            <div className="p-4 text-sm border-y">
-              <p>{props.content3}</p>
+            <div className="p-4 text-sm border-y-2 border-white">
+              {profContents.map((prof) => (
+                <p key={prof.id}>{prof.content}</p>
+              ))}
+              {/* <p>{props.content3}</p>
               <p>{props.content4}</p>
-              <p>{props.content5}</p>
+              <p>{props.content5}</p> */}
             </div>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
-import type { NextPage } from 'next';
-
 import Layout from '@/components/Layout';
 import Card from '@/components/profile/Card';
 import Careers from '@/components/profile/Careers';
 import Works from '@/components/profile/Works';
+import type { NextPage } from 'next';
+import Data from '../data/Data.json';
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <div className="mx-auto w-3/5">
-        <Card
-          content1="山口大陽"
-          content2="Taiyo Yamaguchi"
-          content3="出身：静岡県藤枝市"
-          content4="生年月日:1999年6月22日"
-          content5="趣味:麻雀"
-        />
+        <Card profContents={Data.profData} />
         <Careers />
         <Works />
       </div>
