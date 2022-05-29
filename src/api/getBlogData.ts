799956d0c3f781ac7975bffc0617e67f770b232a
onikun94 <onikun94@gmail.com>
2022-05-30 07:17:40 +0900

    divide getdata

--- src/api/getBlogData.ts
+++ src/api/getBlogData.ts
@@ -1,7 +1,13 @@
-import { TitleBlogType } from '@/types/BlogType';
+import { BlogAllType, BlogType } from '@/types/BlogType';
 
-export const getBlogData = async (url: string): Promise<TitleBlogType[]> => {
+export const getBlogData = async (url: string): Promise<BlogAllType> => {
   const res: Response = await fetch(url);
-  const _res: TitleBlogType[] = await res.json();
+  const _res: BlogAllType = await res.json();
+  return _res;
+};
+
+export const getOnlyBlogData = async (url: string): Promise<BlogType> => {
+  const res: Response = await fetch(url);
+  const _res: BlogType = await res.json();
   return _res;
 };
