2af0aa689782933988893b42b35b2550a2f8976d
onikun94 <onikun94@gmail.com>
2022-05-30 07:03:49 +0900

    add domain for image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ['onikunblog.herokuapp.com'],
+  },
   reactStrictMode: true,
   experimental: {
     optimizeFonts: true,
