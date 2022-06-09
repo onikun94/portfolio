8e38af739792c14c230f517536c9d4a828621a55
onikun94 <onikun94@gmail.com>
2022-06-09 23:33:06 +0900

    add margin

--- src/components/profile/Career.tsx
+++ src/components/profile/Career.tsx
@@ -9,7 +9,7 @@ const Career: VFC<CareerPageProps> = (props) => {
   return (
     <div className="pb-1 mx-8 ">
       <p className="my-1 text-sm text-gray-600">{props.date}</p>
-      <p className="font-sanjp text-sm">{props.contents}</p>
+      <p className=" m-4 font-sanjp text-sm">{props.contents}</p>
     </div>
   );
 };
