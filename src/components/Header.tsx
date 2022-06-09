86ba77a80283184943afc8f75cf4b533d8326996
onikun94 <onikun94@gmail.com>
2022-06-09 23:49:36 +0900

    del import use~

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState, VFC } from 'react';
+import React, { VFC } from 'react';
 type HeaderTypeProps = {
   title: string;
   snsData: { id: number; site: string; link: string; img: string }[];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 
 import Data from '../data/Data.json';
 
