0b7f25235fdeb87039974281823727bc284eaa7e
onikun94 <onikun94@gmail.com>
2022-06-12 16:28:34 +0900

    set environment variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
+// add envdata to vercel
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
