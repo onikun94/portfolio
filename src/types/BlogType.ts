20a92e29776ab66f41666882c6d8b6bb2cbf51a3
onikun94 <onikun94@gmail.com>
2022-06-12 22:43:28 +0900

    del type

--- src/types/BlogType.ts
+++ src/types/BlogType.ts
@@ -1,26 +1,3 @@
-export type BlogAllType = {
-  count: number;
-  next: string;
-  previous: string;
-  results: TitleBlogType[];
-};
-
-export type BlogType = {
-  id?: number;
-  created_at?: Date;
-  updated_at?: Date;
-  title?: string;
-  contents?: string;
-  image: string;
-};
-
-export type TitleBlogType = {
-  id: number;
-  updated_at: Date;
-  title: string;
-  image: string;
-};
-
 export type SupaDataList = {
   id: number;
   title: string | null;
