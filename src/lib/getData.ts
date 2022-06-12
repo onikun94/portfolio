5665aeb85c0d4198e91af39b6540c8387997d217
onikun94 <onikun94@gmail.com>
2022-06-12 16:08:03 +0900

    add supabase

---
+++ src/lib/getData.ts
@@ -0,0 +1,40 @@
+import { supabase } from './supabaseClient';
+
+import { SupaDataList, SupaList } from '@/types/BlogType';
+
+export async function getAllData(offset: number, limit: number): Promise<SupaList> {
+  const { data, error, count } = await supabase
+    .from<SupaDataList>('onikunBlog')
+    .select(
+      `
+      id,created_at,title
+    `,
+      { count: 'exact' },
+    )
+    .range(offset, limit)
+    .order('created_at', { ascending: false });
+
+  if (error) {
+    throw error;
+  }
+  return { data: data, count: count };
+}
+
+export async function getDetailData(id: string) {
+  const { data, error } = await supabase
+    .from<SupaDataList>('onikunBlog')
+    .select(
+      `
+    id,created_at,title,contents
+        )
+      )
+    `,
+    )
+    .eq('id', Number(id))
+    .single();
+
+  if (error) {
+    throw error;
+  }
+  return data;
+}
